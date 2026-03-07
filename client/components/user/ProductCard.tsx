60046dae063fe33444e7bf97aeae1ddcc1d5d55e
sahiltechpreneur <sahiltechpreneur@gmail.com>
2026-03-07 15:21:46 +0545

    fix: image tag

--- client/components/user/ProductCard.tsx
+++ client/components/user/ProductCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import Button from "@/components/ui/Button"
+import Image from "next/image"
 
 type Props = {
   id: number
@@ -13,7 +14,7 @@ export default function ProductCard({ id, name, image, description, price }: Pro
   return (
     <div className="border rounded-lg p-4 hover:shadow-lg transition">
       <Link href={`/product/${id}`}>
-        <img src={image} alt={name} className="w-full h-48 object-cover rounded cursor-pointer" />
+        <Image src={image} alt={name} className="w-full h-48 object-cover rounded cursor-pointer" />
       </Link>
       <Link href={`/product/${id}`}>
         <h3 className="text-lg font-semibold mt-2 cursor-pointer">{name}</h3>
