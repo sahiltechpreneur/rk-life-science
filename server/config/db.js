5a43c8081505cae63f396638ff876fb7831bd162
sahiltechpreneur <sahiltechpreneur@gmail.com>
2026-03-25 23:24:24 +0545

    fix: db.js for deploy

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "rk_life_science",
-  password: "1121",
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: { 
+    rejectUnauthorized: false
+   } 
 })
 
 module.exports = pool
